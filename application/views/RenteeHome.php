996c115ea900623a908b31f5c9ae53e08510c3bf
LilyPatterson <54870366+LilyPatterson@users.noreply.github.com>
2020-02-27 17:10:27 -0500

    HTML Update

---
+++ application/views/RenteeHome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<title>Rentee Homepage</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="index.css">
+</head>
+<body>
+	<header>
+	<h1>Welcome to MyRents (Rentee)!</h1>
+	</header>
+	<a class="button-one" href="#">Make a Payment</a>
+    <a class="button-two" href="#">Submit Work Order</a>
+    <a class="button-three" href="#">View History</a>
+</body>
+</html>
