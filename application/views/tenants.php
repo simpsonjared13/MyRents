d02cf605f99bcd6754649aac400c875488610c64
mulleea <55160617+mulleea@users.noreply.github.com>
2020-03-05 18:13:43 -0500

    Update tenants.php

--- application/views/tenants.php
+++ application/views/tenants.php
@@ -1,4 +1,17 @@
-<div class="wrapper_1">
-	<div class="homepage_box_left">
-	<p>Tennants</p>
+<div class="container">
+	<h1 style="text-align: center;">Tenants</h1>
+	<div class="wrapper_1">
+		<div class="box_first_row">
+			<p>insert new tenants</p>
+		</div>
+
+
+	</div>
+
+	<div class="wrapper_2">
+		<div class="box_second_row">
+			<p>view and update</p>
+		</div>
+
+	</div>
 </div>
