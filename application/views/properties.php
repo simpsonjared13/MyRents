a7cf1580e88ae23474c2faa3b266e8393095ab88
Jared Simpson <simpsonjared13@gmail.com>
2020-03-12 18:03:03 -0400

    units added!

--- application/views/properties.php
+++ application/views/properties.php
@@ -9,7 +9,19 @@
 				State:<input type="text" name="state">
 				County:<input type="text" name="county">
 				Zip:<input type="text" name="zip">
-				Number of Units:<input type="number" name="num_units">
+				Number of Units:<select id="units" name="num_units" onchange="unitsStuff()">
+			<?php
+				for($i = 0; $i <= 5; $i++){
+					echo '<option value="'.$i.'">'.$i.'</option>';
+				}
+			?>
+				</select>
+				<br>
+			<div class="hidden" id='property_units'>
+				<div id="inputs">
+				</div>
+			</div>
+
 				<input type="submit" name="subimt">
 			</form>
 		</div>
@@ -38,8 +50,27 @@
 					</tr>
 				<?php endforeach; ?>
 			</table>
-
 		</div>
-
 	</div>
 </div>
+<script type="text/javascript">
+	function unitsStuff(){
+		var element = document.getElementById('units').value;
+		if(element > 0){
+			var str ='';
+			for (var i = 1; i <= element; i++) {
+				str += 'Unit Number:<input type="text" name="unit_num_'+i+'">\
+						Rent:<input type="text" name="rent_'+i+'"><br>';
+			}
+			var h = document.getElementById("property_units");
+			var inputs = document.getElementById("inputs");
+			if(inputs == null){
+				h.insertAdjacentHTML("afterbegin", str);	
+			}
+			else{
+				h.innerHTML = '';
+				h.insertAdjacentHTML("afterbegin", str);	
+			}
+		}
+	}
+</script>
