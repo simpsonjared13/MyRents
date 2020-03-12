435056c168569f257f6bcee4f3f8d608d007b1c6
Jared Simpson <simpsonjared13@gmail.com>
2020-03-12 17:02:39 -0400

    updated rentee form

--- application/views/tenants.php
+++ application/views/tenants.php
@@ -4,7 +4,20 @@
 		<div class="box_first_row">
 			<h3>Insert New Tenant</h3>
 			<form method="POST">
-				Rentee ID: <input type="text" name="rentee_id" placeholder="">
+				<input type="text" name="first_name" placeholder="first name" required>
+				<input type="text" name="last_name" placeholder="last name" required>
+				<input type="text" name="username" placeholder="username" required>
+				<input type="text" name="email" placeholder="email" required>
+				<input type="text" name="phone" placeholder="203-987-6543">
+				<select name="property_id">
+					<option value="">Property 1</option>
+					<option value="">Property 2</option>
+				</select>
+				<select name="unti_id">
+					<option value="">Unit 1</option>
+					<option value="">Unit 2</option>
+				</select>
+
 				<input type="submit" name="Submit">
 			</form>
 		</div>
