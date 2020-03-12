2fa9141237b54af594d085b31029c03651de9397
Jared Simpson <simpsonjared13@gmail.com>
2020-03-12 16:06:56 -0400

    Update tenants.php

--- application/views/tenants.php
+++ application/views/tenants.php
@@ -2,7 +2,11 @@
 	<h1 style="text-align: center;">Tenants</h1>
 	<div class="wrapper_1">
 		<div class="box_first_row">
-			<p>insert new tenants</p>
+			<h3>Insert New Tenant</h3>
+			<form method="POST">
+				Rentee ID: <input type="text" name="rentee_id" placeholder="">
+				<input type="submit" name="Submit">
+			</form>
 		</div>
 
 
