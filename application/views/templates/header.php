dc86aa52c2d24b15e6516921e7a7846906aa849c
Jared Simpson <simpsonjared13@gmail.com>
2020-03-21 14:56:32 -0400

    Fixed styles to work on my computer

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,6 +8,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>MyRents</title>
-	<link rel="stylesheet" type="text/css" href="/MyRents/assets/styles.php"/>
+	<link rel="stylesheet" href="<?php base_url();?>/MyRents/assets/styles.css"/>
 </head>
-<body>
+<body> 
